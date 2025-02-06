2753f8e1c0cc1bb68b1de9f85c57a250a226155e
osmanBAyn <osbaran10@gmail.com>
2025-02-06 15:27:28 +0300

    Added Alg Commands

---
+++ src/main/java/frc/robot/commands/GetAlg.java
@@ -0,0 +1,19 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.RobotContainer;
+
+public class GetAlg {
+    private RobotContainer robotContainer;
+    public GetAlg(RobotContainer robotContainer){
+        this.robotContainer = robotContainer;
+    }
+    public Command FromGround(){
+        return new SequentialCommandGroup(new ParallelCommandGroup(robotContainer.getElevator().SetElevAndWaitUntilFinishes(1),robotContainer.getArm().setPositionAndWait(1, 1)),robotContainer.getGripper().pullAlg());
+    }
+    public Command FromReef(){
+        return new SequentialCommandGroup(new ParallelCommandGroup(robotContainer.getElevator().SetElevAndWaitUntilFinishes(1),robotContainer.getArm().setPositionAndWait(1, 1)),robotContainer.getGripper().pullAlg());
+    }
+}
---
+++ src/main/java/frc/robot/commands/PutAlg.java
@@ -0,0 +1,19 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.RobotContainer;
+
+public class PutAlg {
+    private RobotContainer robotContainer;
+    public PutAlg(RobotContainer robotContainer){
+        this.robotContainer = robotContainer;
+    }
+    public Command ToBarge(){
+        return new SequentialCommandGroup(new ParallelCommandGroup(robotContainer.getElevator().SetElevAndWaitUntilFinishes(1),robotContainer.getArm().setPositionAndWait(1, 1)),robotContainer.getGripper().pushAlg());
+    }
+    public Command ToProcessor(){
+        return new SequentialCommandGroup(new ParallelCommandGroup(robotContainer.getElevator().SetElevAndWaitUntilFinishes(1),robotContainer.getArm().setPositionAndWait(1, 1)),robotContainer.getGripper().pushAlg());
+    }
+}
